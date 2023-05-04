1d147a90db521314d18689cfa525584488d3f9d1
cdnize <132540269+cdnize@users.noreply.github.com>
2023-05-04 01:43:52 -0400

    Update index.js

--- .github/actions/fetch-json-write-file/dist/index.js
+++ .github/actions/fetch-json-write-file/dist/index.js
@@ -8556,7 +8556,7 @@ const fs = __nccwpck_require__(7147);
       console.log(e);
     }
     // This output also isn't needed anymore as we are doing the successful check inside this script when we check that filter.length isn't 0
-    core.setOutput(database + "-successful", successful);
+    // core.setOutput(database + "-successful", successful);
     console.log("action() - successful=" + successful);
   }
 
