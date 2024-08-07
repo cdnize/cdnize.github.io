5ebd8c8131ac005d9608ba5b17fa39e7237909cd
cdnize <132540269+cdnize@users.noreply.github.com>
2024-08-06 21:24:21 -0400

    Update index.js

--- .github/actions/fetch-json-write-file/index.js
+++ .github/actions/fetch-json-write-file/index.js
@@ -11,7 +11,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
 
-// import fetch from 'node-fetch';
+import fetch from 'node-fetch';
 
 (async () => {
 
